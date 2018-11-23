918832b8a72a81cbd88d771547bd2824e5152896
Максим <@DESKTOP-00TF1MS>
2018-11-23 18:23:06 +0200

    startet pac

---
+++ militarism2/src/main/java/com/militarism2/militarism2/Militarism2Application.java
@@ -0,0 +1,12 @@
+package com.militarism2.militarism2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Militarism2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Militarism2Application.class, args);
+	}
+}
