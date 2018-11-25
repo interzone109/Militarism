dde646bf286e776b5dec721e295beddd140d80f2
interzone <interzone@gmail.com>
2018-11-25 20:12:02 +0200

    prop

--- militarism2/src/main/java/com/militarism2/militarism2/model/User.java
+++ militarism2/src/main/java/com/militarism2/militarism2/model/User.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Type;
+
 @Entity
 @Table(name = "users")
 public class User {
