fc2c4e101d205aa94ab10496c619d7bb0a41d531
Diman96 <nde96m@gmail.com>
2018-11-28 14:43:41 +0200

    Руслан

--- militarism2/src/main/java/com/militarism2/militarism2/repozitory/UserRepository.java
+++ militarism2/src/main/java/com/militarism2/militarism2/repozitory/UserRepository.java
@@ -9,4 +9,5 @@ import com.militarism2.militarism2.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findOneByEmail(String email);
 	Optional<User> findOneByName(String name);
+	//руслан
 }
