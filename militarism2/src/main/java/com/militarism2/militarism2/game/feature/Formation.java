e0b51fbe0b59365805cbf9655ce2ab40cc2f87aa
interzone <interzone@gmail.com>
2018-11-28 16:45:52 +0200

    Formation

---
+++ militarism2/src/main/java/com/militarism2/militarism2/game/feature/Formation.java
@@ -0,0 +1,36 @@
+package com.militarism2.militarism2.game.feature;
+
+import lombok.Data;
+
+/**
+ * Класс Formation описывает такие данные как
+ * количество юнитов в отряде, их защина и опыт
+ * */
+@Data
+public class Formation {
+	private int quantityUnits;
+	private int defense;
+	private int expirience;
+	
+	
+	public Formation(int quantityUnits, int defense, int expirience) {
+		super();
+		this.quantityUnits = quantityUnits;
+		this.defense = defense;
+		this.expirience = expirience;
+	}
+
+	
+
+	public Formation clone() {
+		return new Formation(quantityUnits, defense, expirience) ;
+	}
+	
+	@Override
+	public String toString() {
+		return "quantity units:"+quantityUnits
+				   +" defense:"+defense
+				   +" expirience:"+ expirience+"\n";
+	}
+
+}
