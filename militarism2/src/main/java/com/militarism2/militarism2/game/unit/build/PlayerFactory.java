54b4729a8c5f26115d556b212f2d9863347e7a70
interzone <interzone@gmail.com>
2018-12-04 12:28:57 +0200

    update factory

--- militarism2/src/main/java/com/militarism2/militarism2/game/unit/build/PlayerFactory.java
+++ militarism2/src/main/java/com/militarism2/militarism2/game/unit/build/PlayerFactory.java
@@ -14,9 +14,9 @@ import com.militarism2.militarism2.game.unit.army.template.*;
 public class PlayerFactory {
 	private String country;
 	private Point location;
-	private Map<String, TemplateBreak> productLine;
+	private Map<String, Template> productLine;
 
-	public PlayerFactory(String country, Point location, Map<String, TemplateBreak> productLine) {
+	public PlayerFactory(String country, Point location, Map<String, Template> productLine) {
 		this.country = country;
 		this.location = location;
 		this.productLine = productLine;
@@ -24,49 +24,49 @@ public class PlayerFactory {
 
 	public Infantry creatInfantry(String name) throws ClassNotFoundException {
 		if (productLine.containsKey(name)) {
-		return new Infantry(productLine.get(name));
+		return new Infantry(productLine.get(name), location);
 		}
 		throw new ClassNotFoundException("Type :" + name + " not exist, check the spelling");
 		}
 
 	public Tank creatTank(String name) throws Exception {
 		if (productLine.containsKey(name)) {
-			return new Tank(productLine.get(name));
+			return new Tank(productLine.get(name), location);
 		}
 		throw new ClassNotFoundException("Type :" + name + " not exist, check the spelling");
 	}
 
 	public Artillery creatArtillery(String name) throws ClassNotFoundException {
 		if (productLine.containsKey(name)) {
-			return new Artillery(productLine.get(name));
+			return new Artillery(productLine.get(name), location);
 		}
 		throw new ClassNotFoundException("Type :" + name + " not exist, check the spelling");
 	}
 
 	public Airplane creatAirplane(String name) throws ClassNotFoundException {
 		if (productLine.containsKey(name)) {
-			return new Airplane(productLine.get(name));
+			return new Airplane(productLine.get(name), location);
 		}
 		throw new ClassNotFoundException("Type :" + name + " not exist, check the spelling");
 	}
 
 	public Shipe creatShipe(String name) throws ClassNotFoundException {
 		if (productLine.containsKey(name)) {
-			return new Shipe(productLine.get(name));
+			return new Shipe(productLine.get(name), location);
 		}
 		throw new ClassNotFoundException("Type :" + name + " not exist, check the spelling");
 	}
 
 	public Submarine creatSubmarine(String name) throws ClassNotFoundException {
 		if (productLine.containsKey(name)) {
-			return new Submarine(productLine.get(name));
+			return new Submarine(productLine.get(name), location);
 		}
 		throw new ClassNotFoundException("Type :" + name + " not exist, check the spelling");
 	}
 
 	public Support creatSupport(String name) throws ClassNotFoundException {
 		if (productLine.containsKey(name)) {
-			return new Support(productLine.get(name));
+			return new Support(productLine.get(name), location);
 		}
 		throw new ClassNotFoundException("Type :" + name + " not exist, check the spelling");
 	}
