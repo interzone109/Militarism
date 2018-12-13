e739d96424d1a9657539cc8c89a7c262cdec60a0
interzone <interzone@gmail.com>
2018-12-13 12:49:34 +0200

    add creator

--- militarism2/src/main/java/com/militarism2/militarism2/controller/TemplateCreatorController.java
+++ militarism2/src/main/java/com/militarism2/militarism2/controller/TemplateCreatorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.militarism2.militarism2.game.unit.service.TemplateServiceImpl;
+import com.militarism2.militarism2.game.unit.type.ArmyType;
 import com.militarism2.militarism2.mvc.model.TempalteModel;
 
 @Controller
--- militarism2/src/main/java/com/militarism2/militarism2/game/map/MapPoint.java
+++ militarism2/src/main/java/com/militarism2/militarism2/game/map/MapPoint.java
@@ -1,10 +1,18 @@
 package com.militarism2.militarism2.game.map;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.militarism2.militarism2.game.unit.type.Direction;
@@ -12,28 +20,32 @@ import com.militarism2.militarism2.game.unit.type.Terrain;
 
 import lombok.Data;
 
-@Entity
-@Table(name = "map_points")
+/*@Entity
+@Table(name = "map_points")*/
 @Data
 public class MapPoint {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "map_point_Id", nullable = false, updatable = false, unique = true)
+  //	@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@Column(name = "map_point_Id", nullable = false, updatable = false, unique = true)
 	private Long id;
-	@Column(name = "offset_X")
+	//@Column(name = "offset_X")
 	private int offsetX;
-	@Column(name = "offset_Y")
+	//@Column(name = "offset_Y")
 	private int offsetY;
-
+	//@Enumerated(EnumType.ORDINAL) 
 	Terrain terrain;
-	Direction [] direction;
+	
+/*	@ElementCollection(targetClass=Direction.class)
+	@Enumerated(EnumType.STRING) 
+	@OneToMany( fetch = FetchType.EAGER, cascade=CascadeType.ALL)*/
+	List<Direction> direction;
 
-	public MapPoint(int offsetX, int offsetY, Terrain terrain, Direction []direction) {
+/*	public MapPoint(int offsetX, int offsetY, Terrain terrain, Direction []direction) {
 		this.offsetX = offsetX;
 		this.offsetY = offsetY;
 		this.terrain = terrain;
 		this.direction = direction;
-	}
+	}*/
 
 	public String toString() {
 		return "x-" + offsetX + " y-" + offsetY + " terrain-" + terrain;
